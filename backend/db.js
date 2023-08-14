926f876de87434f957ebbfc0036a29d7d67f41ff
aomwankhede <wankhede.aom@gmail.com>
2023-08-14 23:30:01 +0530

    validation using express-validator module

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 main().then(()=>{console.log("Connected successfully!")}).catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017');
+  await mongoose.connect('mongodb://127.0.0.1:27017/iNotebook');
 }
 
 module.exports=main;
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,11 +6,21 @@ const router=express.Router();
 //create a user using :POST '/api/auth' .Doesnt erequire auth
 
 // If we send the data using get we may fimnd our data on the log file
-router.post('/',(req,res)=>{
-    console.log(req.body);
-    const user=User(req.body);
-    user.save();
-    res.send(req.body);
+router.post('/',[
+    //second argument is the custom message
+    body('name','Enter a valid name').isLength({min:3}),
+    body('email','Enter a valid email').isLength({min:3}),
+    body('password','Enter a valid password').isLength({min:5}),
+],(req,res)=>{
+    const errors=validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()});
+    }
+    User.create({
+        name:req.body.name,
+        email:req.body.email,
+        password:req.body.password
+    }).then((user)=>{res.json(user)}).catch((err)=>{console.log(err);res.json({"mnessage":"Enter valid data"})})
 })
 
 module.exports=router
