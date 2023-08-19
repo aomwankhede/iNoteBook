031ec51219ab46a222f14704232b79ac4b5f5491
aomwankhede <wankhede.aom@gmail.com>
2023-08-19 19:39:50 +0530

    Adding more frontend to notesItem

--- frontend/src/components/NoteItem.js
+++ frontend/src/components/NoteItem.js
@@ -5,12 +5,14 @@ function NoteItem(props) {
     <>
       <div className="card my-3 mx-4" style={{ width: "30rem" ,border:'2px solid black'}}>
         <div className="card-body">
-          <h5 className="card-title">{props.note.title}</h5>
+          <h5 className="card-title" style={{fontWeight:'bolder',fontSize:'25px'}}>{props.note.title}</h5>
           <h6 className="card-subtitle mb-2 text-muted">{props.note.tag}</h6>
           <p className="card-text">{props.note.description}</p>
-          <p href="#" className="card-link">
+          <p href="#" className="card-link" style={{fontWeight:'bold'}}>
             {props.note.date}
           </p>
+            <i className="fa-solid fa-trash fa-lg"></i>
+            <i class="fa-solid fa-pen-to-square fa-lg" style={{position:'relative',float:'right'}}></i>
         </div>
       </div>
     </>
